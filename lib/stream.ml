7b999feaa782315607de62a6428816af04665486
Nicolas Paul <n@nc0.fr>
2025-04-01 20:37:52 +0200

    Add documentation for stream module
    The stream module lacked documentation about the construction of a
    stream and of the different functions exposed by the module.  Some
    questions remain unanswered for now, this will be discussed during
    meetings.

--- lib/stream.ml
+++ lib/stream.ml
@@ -1,74 +1,95 @@
-
 (*************************************************************************)
 (*                                                                       *)
 (*                                OCamlFRP                               *)
 (*                                                                       *)
 (* Copyright (C) 2025  Frédéric Dabrowski                                *)
-(* All rights reserved.  This file is distributed under the terms of      *)
+(* All rights reserved.  This file is distributed under the terms of     *)
 (* the GNU Lesser General Public License version 3.                      *)
 (* You should have received a copy of the GNU General Public License     *)
 (* along with this program.  If not, see <https://www.gnu.org/licenses/>.*)
 (*************************************************************************)
 
-type 'a stream = 
-  | Str : ('state -> ('a * 'state)) * 'state -> 'a stream
+type 'a stream = Str : ('state -> 'a * 'state) * 'state -> 'a stream
 
-let destr : 'a stream -> 'a * 'a stream = 
-    fun (Str (f,s)) ->
-      let (a,s') = f s in (a, Str (f,s'))
+let destr : 'a stream -> 'a * 'a stream =
+  fun (Str (f, s)) ->
+  let a, s' = f s in
+  a, Str (f, s')
+;;
 
-let head : 'a stream -> 'a =
-  fun s -> fst (destr s) 
+let head : 'a stream -> 'a = fun s -> fst (destr s)
+let tail : 'a stream -> 'a stream = fun s -> snd (destr s)
 
-let tail : 'a stream -> 'a stream =
-  fun s -> snd (destr s)
-  
-let map : 'a 'b. ('a -> 'b) -> 'a stream -> 'b stream = 
-  fun f (Str (h,s)) -> 
-    Str ((fun s -> let (a,s') = h s in (f a, s')), s)
+let map : 'a 'b. ('a -> 'b) -> 'a stream -> 'b stream =
+  fun f (Str (h, s)) ->
+  Str
+    ( (fun s ->
+        let a, s' = h s in
+        f a, s')
+    , s )
+;;
 
 let apply : 'a 'b. ('a -> 'b) stream -> 'a stream -> 'b stream =
   fun (Str (f, i)) (Str (e, ie)) ->
-    Str ((fun (sf,se) -> 
-      let (vf,sf') = f sf in
-      let (ve,se') = e se in
-        ((vf ve), (sf', se'))),
-        (i,ie))
-  
+  Str
+    ( (fun (sf, se) ->
+        let vf, sf' = f sf in
+        let ve, se' = e se in
+        vf ve, (sf', se'))
+    , (i, ie) )
+;;
 
-let produce : 'a 's. ('s -> 'a * 's) -> 's -> 'a stream = 
-  fun h s -> Str (h,s) 
+let produce : 'a 's. ('s -> 'a * 's) -> 's -> 'a stream = fun h s -> Str (h, s)
 
-let coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream = 
-  fun f x0 -> produce (fun x -> let y = f x in (x,y)) x0
+let coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream =
+  fun f x0 ->
+  produce
+    (fun x ->
+       let y = f x in
+       x, y)
+    x0
+;;
 
-let constant : 'a. 'a -> 'a stream =
-  fun x -> coiterate (Fun.const x) x
-  
-let rec perform : 'a stream -> ('a -> unit) -> int -> unit = 
+let constant : 'a. 'a -> 'a stream = fun x -> coiterate (Fun.const x) x
+
+(* NOTE(nico): does it assumes f has side-effects? should it be documented? *)
+let rec perform : 'a stream -> ('a -> unit) -> int -> unit =
   fun s f n ->
-    if n <= 0 then ()
-    else 
-      let (Str (h,s)) =  s in 
-      let (a,s') = h s in f a; 
-        perform (Str (h,s')) f (n-1)
+  if n <= 0
+  then ()
+  else (
+    let (Str (h, s)) = s in
+    let a, s' = h s in
+    f a;
+    perform (Str (h, s')) f (n - 1))
+;;
 
-let rec consume : 'a stream -> ('a -> bool) -> float option -> unit = 
+let rec consume : 'a stream -> ('a -> bool) -> float option -> unit =
   fun s f d ->
-    let (Str (h,s)) =  s in 
-    let (a,s') = h s in 
-    let b = f a in
-      (* print_string "a\n"; flush stdout; *)
-      match d with None -> () | Some t -> Thread.delay t;
-      if b then 
-        consume (Str (h,s')) f d
-      else ()   
+  let (Str (h, s)) = s in
+  let a, s' = h s in
+  let b = f a in
+  (* print_string "a\n"; flush stdout; *)
+  match d with
+  | None -> ()
+  | Some t ->
+    Thread.delay t;
+    if b then consume (Str (h, s')) f d else ()
+;;
 
-let stream_of_list (l : 'a list) (a :'a) : 'a stream =
-  Str ((fun l -> match l with [] -> (a,l) | h::t -> (h, t)), l)
-      
-let rec list_of_stream (Str (f,i) : 'a stream) (n : int) = 
-  if n > 0 then 
-    let (a, s') = f i in 
-      a::(list_of_stream (Str (f, s')) (n-1))
+let stream_of_list (l : 'a list) (a : 'a) : 'a stream =
+  Str
+    ( (fun l ->
+        match l with
+        | [] -> a, l
+        | h :: t -> h, t)
+    , l )
+;;
+
+let rec list_of_stream (Str (f, i) : 'a stream) (n : int) =
+  if n > 0
+  then (
+    let a, s' = f i in
+    a :: list_of_stream (Str (f, s')) (n - 1))
   else []
+;;
--- lib/stream.mli
+++ lib/stream.mli
@@ -3,7 +3,8 @@
 (*                                OCamlFRP                               *)
 (*                                                                       *)
 (* Copyright (C) 2025  Frédéric Dabrowski                                *)
-(* All rights reserved.  This file is distributed under the terms of      *)
+(* Copyright (C) 2025  Nicolas Paul                                      *)
+(* All rights reserved.  This file is distributed under the terms of     *)
 (* the GNU Lesser General Public License version 3.                      *)
 (* You should have received a copy of the GNU General Public License     *)
 (* along with this program.  If not, see <https://www.gnu.org/licenses/>.*)
@@ -11,39 +12,55 @@
 
 (** {1 Streams} *)
 
-(** ['a stream] represents a type of stream of values of type ['a], 
-    which is an infinite sequence of values of type ['a]. *)
+(** ['a stream] represents a type of stream of values of type ['a], which is an infinite
+    sequence of values of type ['a].  It is a pair whose first value is a generator
+    function which given a state returns a value and the next state, and second value
+    is the initial state in the stream. *)
+type 'a stream = Str : ('s -> 'a * 's) * 's -> 'a stream
 
-type 'a stream = Str : ('s -> ('a * 's)) * 's -> 'a stream
-
+(** [destr s] deconstructs stream [s] into a pair containing its head and its tail. *)
 val destr : 'a stream -> 'a * 'a stream
 
+(** [head s] returns the head of stream [s]. *)
 val head : 'a stream -> 'a
-  
-val tail : 'a stream -> 'a stream 
 
+(** [head s] returns the tail of stream [s]. *)
+val tail : 'a stream -> 'a stream
+
+(** [map f s] constructs a stream by applying function [f] to values of stream [s]. *)
 val map : 'a 'b. ('a -> 'b) -> 'a stream -> 'b stream
-  
+
+(** [apply f s] constructs a new stream by applying functions from stream of function
+    [f] to the values of stream [s]. *)
 val apply : 'a 'b. ('a -> 'b) stream -> 'a stream -> 'b stream
-  
+
+(** [produce g s] constructs a stream from generator function [g] and
+    initial state [s]. *)
+val produce : ('s -> 'a * 's) -> 's -> 'a stream
+
+(* TODO(nico): ????
+(** [fold f x] generates a stream by iteratively applying the function [f] 
+    to the current value, starting with [x]. s*)
+*)
 
 (** [coiterate f s] constructs a stream using the coiterator [(f, s)], 
     where [f] is the function and [s] is the initial state. *)
-val produce : ('s -> 'a * 's) -> 's -> 'a stream
+val coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream
 
-(** [fold f x] generates a stream by iteratively applying the function [f] 
-    to the current value, starting with [x]. s*)
-    val coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream
-    
 (** [constant x] returns a constant stream where every element has the value x. *)
-    val constant : 'a. 'a -> 'a stream
-    
-    val perform : 'a stream -> ('a -> unit) -> int -> unit
+val constant : 'a. 'a -> 'a stream
 
-    val consume : 'a stream -> ('a -> bool) -> float option -> unit
-    (* hd, tl ?? *)
+(** [perform s f n] consumes stream [s], applying function [f] on [n] values of [s]. *)
+val perform : 'a stream -> ('a -> unit) -> int -> unit
 
+(** [consume s p d] consumes the values from stream [s] as long as the values validates
+    predicates [p].  An optional delay [d] can be passed to wait sleep between two
+    consumption. *)
+val consume : 'a stream -> ('a -> bool) -> float option -> unit
+
+(** [stream_of_list l a] constructs a stream from the elements of list [l], followed
+    by value [a] once every element of [l] was streamed. *)
 val stream_of_list : 'a list -> 'a -> 'a stream
-    
-val list_of_stream : 'a stream -> int -> 'a list 
-    
+
+(** [list_of_stream s n] constructs a list of [n] values from stream [s]. *)
+val list_of_stream : 'a stream -> int -> 'a list
