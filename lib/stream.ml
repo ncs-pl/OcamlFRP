a93bd735026307def31daa9cb7a34a1ee172fcb1
Nicolas Paul <n@nc0.fr>
2025-04-17 12:23:02 +0200

    Use more algebraic coding style

--- lib/stream.ml
+++ lib/stream.ml
@@ -12,83 +12,74 @@
 
 type 'a stream = Str : ('state -> 'a * 'state) * 'state -> 'a stream
 
-let destr s =
-  let (Str (gen, init)) = s in
-  let value, next = gen init in
-  value, Str (gen, next)
+let destr (Str (f, s)) =
+  let x, s' = f s in
+  x, Str (f, s')
 ;;
 
 let head s = fst (destr s)
 let tail s = snd (destr s)
 
-let map f s =
-  let (Str (s_gen, s_init)) = s in
-  let gen state =
-    let v, next = s_gen state in
-    f v, next
+let map f (Str (g, g0)) =
+  let gen s =
+    let x, s' = g s in
+    f x, s'
   in
-  Str (gen, s_init)
+  Str (gen, g0)
 ;;
 
-let apply f s =
-  let (Str (f_gen, f_init)) = f in
-  let (Str (s_gen, s_init)) = s in
-  let gen (f_state, s_state) =
-    let f_value, next_f_state = f_gen f_state in
-    let s_value, next_s_state = s_gen s_state in
-    f_value s_value, (next_f_state, next_s_state)
+let apply (Str (f, f0)) (Str (v, v0)) =
+  let h (s1, s2) =
+    let x1, s1' = f s1 in
+    let x2, s2' = v s2 in
+    x1 x2, (s1', s2')
   in
-  let init = f_init, s_init in
-  Str (gen, init)
+  let h0 = f0, v0 in
+  Str (h, h0)
 ;;
 
 let produce g s = Str (g, s)
 
 let coiterate f s =
-  let gen x =
+  let h x =
     let x' = f x in
     x, x'
   in
-  produce gen s
+  produce h s
 ;;
 
 let constant x = coiterate (Fun.const x) x
 
-let rec perform s f n =
+let rec perform (Str (g, g0)) f n =
   if n > 0
   then (
-    let (Str (gen, init)) = s in
-    let value, next = gen init in
-    f value;
-    perform (Str (gen, next)) f (n - 1))
+    let x, s' = g g0 in
+    f x;
+    perform (Str (g, s')) f (n - 1))
 ;;
 
-let rec consume s p d =
-  let (Str (gen, init)) = s in
-  let value, next = gen init in
-  let bool = p value in
+let rec consume (Str (f, f0)) p d =
+  let value, next = f f0 in
   match d with
   | None -> ()
   | Some t ->
     Thread.delay t;
-    if bool then consume (Str (gen, next)) p d
+    if p value then consume (Str (f, next)) p d
 ;;
 
 let stream_of_list l a =
-  let gen list =
-    match list with
-    | [] -> a, list
+  let h = function
+    | [] as t -> a, t
     | h :: t -> h, t
   in
-  Str (gen, l)
+  Str (h, l)
 ;;
 
-let rec list_of_stream s n =
-  let (Str (gen, init)) = s in
+let rec list_of_stream (Str (f, s)) n =
   if n > 0
   then (
-    let value, next = gen init in
+    let x, s' = f s in
     (* TODO(nico): final recursion to obtain tail-call optimization *)
-    value :: list_of_stream (Str (gen, next)) (n - 1))
+    x :: list_of_stream (Str (f, s')) (n - 1))
   else []
 ;;
